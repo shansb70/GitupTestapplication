3efaff190d4a4f82a50b73c1b6e185fc9a6f46f2
shansb70 <sundar_sb70@hotmail.com>
2022-07-27 12:13:43 +0530

    Add project files.

---
+++ DemoApplication/Controllers/EmployeeController.cs
@@ -0,0 +1,31 @@
+﻿using Employee.Data;
+using Employee.Data.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoApplication.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult CreateEmployeeDetails(EmployeeData employeeData)
+        {
+            if (employeeData.EmpFirstName != null)
+            {
+                EmployeeModel EmpModel = new EmployeeModel();
+                string strresult = EmpModel.CreateEmployeeDetails(employeeData);
+                ViewData["result"] = strresult;
+                strresult = "";
+            }
+            return View();
+        }
+    }
+}
---
+++ DemoApplication/Models/EmployeeModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoApplication.Models
+{
+    public class EmployeeModel
+    {
+
+        public string EmpFirstName { get; set; }
+
+        public string EmpLastName { get; set; }
+
+        public string EmpDepartment { get; set; }
+
+    }
+}
